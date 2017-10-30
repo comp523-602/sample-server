ae885c5a1a4236f8c5ace57786d58df85e006946
Tomas Roy <hello@minicreative.net>
2017-10-29 20:45:43 -0400

    server.js updated

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var server = express();
 
-server.listen(5000, "165.227.73.61", function () {
-    console.log('Serving demo server on 165.227.73.61:5000...');
+server.listen(5063, "165.227.73.61", function () {
+    console.log('Serving demo server on 165.227.73.61:5063...');
 });
 
 server.get('*', function (req, res) {
-    res.send('hello world!');
+    res.send('hello '+req.params.name+'!');
 });
